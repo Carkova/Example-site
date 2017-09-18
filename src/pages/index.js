b7d532a5af8042b907223f97ee891bfc5023f12c
Carkova <31981467+Carkova@users.noreply.github.com>
2017-09-18 10:45:48 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new kdlfjkdjkjf site.</p>
+    <p>Welcome to your new qwertyqwertyqwerty site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
